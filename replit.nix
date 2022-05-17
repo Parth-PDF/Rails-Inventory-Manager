227b5cd33692f0f9d0c0b29226e9a8e1ad09ae41
parth-pdf <parthcpatel157@gmail.com>
2022-05-16 21:32:21 -0400

    ruby version fix

--- replit.nix
+++ replit.nix
@@ -1,8 +1,8 @@
 { pkgs }: {
-	deps = [
-        pkgs.ruby_3_0
-        pkgs.rubyPackages_3_0.solargraph
-        pkgs.rufo
-        pkgs.sqlite
-	];
+  deps = [
+    pkgs.ruby_3_1_2
+    pkgs.rubyPackages_3_1_2.solargraph
+    pkgs.rufo
+    pkgs.sqlite
+  ];
 }
